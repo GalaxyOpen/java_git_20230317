db7a21279131271b11cadb95594338faba713b40
GalaxyOpen <keehong11@naver.com>
2023-03-13 16:28:37 +0900

    welcome to new space in Java

---
+++ src/hello/hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello, World");
+		
+		
+
+	}
+
+}
