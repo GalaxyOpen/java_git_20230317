d6baa7f2b2d378069d4bf33082513a569e9ad389
GalaxyOpen <keehong11@naver.com>
2023-03-14 10:49:48 +0900

    구구단 메소드

--- src/ex/Example1.java
+++ src/ex/Example1.java
@@ -4,11 +4,10 @@ public class Example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=2; i<10; i++) {
-		   for(int j = 1; j<10; j++) {
-			   System.out.println(i+"*"+j+"="+(i*j));
-		   }
-		}
+		Example2 ex = new Example2();
+		
+		System.out.println("구구단");
+		ex.numberTable();
 
 	}
 
---
+++ src/ex/Example2.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class Example2 {
+	
+	public void numberTable() {
+		for(int i=2; i<10; i++) {
+			   for(int j = 1; j<10; j++) {
+				  
+			   }
+			}
+	}
+
+}
