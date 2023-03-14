cca7713a227edd47a51b3e5537225a179359fb8f
GalaxyOpen <keehong11@naver.com>
2023-03-14 10:56:21 +0900

    구구 1차 수정본

--- src/ex/Example2.java
+++ src/ex/Example2.java
@@ -5,7 +5,7 @@ public class Example2 {
 	public void numberTable() {
 		for(int i=2; i<10; i++) {
 			   for(int j = 1; j<10; j++) {
-				  
+				   System.out.println(i+"*"+j+"="+(i*j));
 			   }
 			}
 	}
