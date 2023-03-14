26062e421f7dfc53e457e993d0e85bb61df2ce3d
GalaxyOpen <keehong11@naver.com>
2023-03-14 10:42:40 +0900

    구구단 실습

---
+++ src/hello/hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to E-java Space");
+
+	}
+
+}
